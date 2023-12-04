bbee3b642e49f7a61a45b04c5b8fdded00db0089
jyoshin <gleb.parkdes@gmail.com>
2023-12-04 18:16:22 +0600

    Done

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import home, add_task, edit_task, delete_task
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', home, name = 'home'),
+    path('add/', add_task, name = 'add_task'),
+    path('edit/<int:task_id>', edit_task, name = 'edit_task'),
+    path('delete/<int:task_id>/', delete_task, name='delete_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from . models import Task
 
 def home(request):
-    return HttpResponse("Hello, this is home")
+    all_tasks = Task.objects.all()
+    return render(request, 'main/home.html', {'all_tasks': all_tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        completion = request.POST.get('completion') == 'on'
+
+        Task.objects.create(title=title, description=description, completion=completion)
+
+        return redirect('home')
+    
+    return render(request, 'main/add_task.html')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        task.completion = request.POST.get('completion') == 'on'
+        task.save()
+
+        return redirect('home')
+    
+    return render(request, 'main/edit_task.html', {'task': task})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        task.delete()
+        
+        return redirect('home')
+    
+    return render(request, 'main/delete_task.html', {'task': task})
